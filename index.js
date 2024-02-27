44ff1c7b40fa79d385abb832d33245de8035c4aa
TimChinye <timchinye123@gmail.com>
2024-02-27 19:41:13 +0000

    Improvement to dashboard

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware for logging requests (optional)
 app.use((req, res, next) => {
     console.log(`Received ${req.method} request for ${req.url}`);
     next();
@@ -21,27 +20,30 @@ app.get('/login', async (req, res) => {
 
 app.post('/submit-login', async (req, res) => {
   try {
-      const user = req.body; // Assuming you have middleware to parse the request body
+      const user = req.body;
       console.log(user);
 
       const userExistsInDatabase = true; // Temporary - PSQL Connection
       if (userExistsInDatabase) {
           // Redirect to dashboard if user exists
-          res.status(200).json({ success: true, user }); // Include user data
+          res.status(200).json({ body: { user } });
       } else {
           // Handle case when user doesn't exist (e.g., show an error page)
-          res.status(404).send('User not found');
+          res.status(404).json({ error: 'User not found' });
       }
   } catch (error) {
-      // Handle exceptions during redirection
       console.error('Error during login:', error);
-      res.status(500).send('Internal server error');
+      res.status(500).send({ error: 'Internal server error' });
   }
 });
 
 
 app.get('/dashboard', async (req, res) => {
     res.sendFile(resolve(__dirname, 'pages/dashboard.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
 app.listen(port, () => {
--- static/login.js
+++ static/login.js
@@ -1,4 +1,4 @@
-const user = {
+let user = {
     email: '',
     password: '',
     phone: '',
@@ -14,6 +14,11 @@ const password = formElem.querySelector('label[for="password"]');
 const phone = formElem.querySelector('label[for="phone"]');
 const code = formElem.querySelector('label[for="code"]');
 
+const emailInput = formElem.querySelector('#email');
+const passwordInput = formElem.querySelector('#password');
+const phoneInput = formElem.querySelector('#phone');
+const codeInput = formElem.querySelector('#code');
+
 requestCodeElem.addEventListener('click', requestCode);
 btnElem.addEventListener('click', login);
 formElem.addEventListener('keypress', (e) => {
@@ -23,18 +28,24 @@ formElem.addEventListener('keypress', (e) => {
     }
 });
 
+function requestCode() {
+    user.phone = phoneInput.value;
+
+    code.removeAttribute("hidden");
+    code.focus();
+    document.getElementById('feedback').innerHTML = "A text message was sent, please check your phone.";
+}
+
 function login() {
     if (btnElem.hasAttribute("disabled")) return;
 
     document.getElementById('feedback').innerHTML = '';
     if (!email.hasAttribute("hidden") && !password.hasAttribute("hidden")) {
-        const emailInput = formElem.querySelector('#email').value;
-        if (emailInput.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g)?.length == 1) {
-            user.email = emailInput;
+        if (emailInput.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g)?.length == 1) {
+            user.email = emailInput.value;
 
-            const passwordInput = formElem.querySelector('#password').value;
-            if (passwordInput.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g)?.length == 1) {
-                user.password = passwordInput;
+            if (passwordInput.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g)?.length == 1) {
+                user.password = passwordInput.value;
     
                 email.setAttribute("hidden", "");
                 password.setAttribute("hidden", "");
@@ -43,40 +54,54 @@ function login() {
                 phone.focus();
                 btnElem.innerHTML = "Authenticate";
                 btnElem.setAttribute("disabled", "");
+                
+                resetInputDetails();
             } else document.getElementById('feedback').innerHTML = "The password is invalid.";
         } else document.getElementById('feedback').innerHTML = "The email is invalid.";
     } else if (!phone.hasAttribute("hidden") && !code.hasAttribute("hidden")) {
-        user.code = formElem.querySelector('#code').value.replace(/\s+/g, '');
+        user.code = codeInput.value.replace(/\s+/g, '');
 
         fetch('/submit-login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(user),
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Response data:', data);
-        
-                if (data.success) {
-                    // Redirect to dashboard
-                    window.location.href = '/dashboard';
-                } else {
-                    console.error('Login failed:', data.error);
-                    // Handle other scenarios (e.g., show an error message)
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                // Handle network errors or other exceptions
-            });
-        
+        .then(response => response.json())
+        .then(data => {
+            console.log('Response data:', data);
+    
+            if (data.body) {
+                // Redirect to dashboard
+                window.location.href = '/dashboard';
+            } else if (data.error) {
+                resetInputDetails(true);
+            }
+        })
+        .catch(error => {
+            // Handle network errors or other exceptions
+            console.error('Error:', error);
+            resetInputDetails(true);
+        });
     }; 
 };
 
-function requestCode() {
-    user.phone = formElem.querySelector('#phone').value;
+function resetInputDetails(fully) {
+    emailInput.value = '';
+    passwordInput.value = '';
+    phoneInput.value = '';
+    codeInput.value = '';
 
-    code.removeAttribute("hidden");
-    code.focus();
-    document.getElementById('feedback').innerHTML = "A text message was sent, please check your phone.";
+    if (fully) {
+        email.removeAttribute("hidden");
+        password.removeAttribute("hidden");
+        phone.setAttribute("hidden", "");
+        code.setAttribute("hidden", "");
+
+        user = {
+            email: '',
+            password: '',
+            phone: '',
+            code: ''
+        };
+    }
 }
